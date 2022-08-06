88dc12ceb10354074ff984c721dfaaeb4f5877aa
Tom Rodriguez <tmrodrig@gmail.com>
2022-08-06 16:45:09 -0700

    Change welcome journal

--- scripts/init.js
+++ scripts/init.js
@@ -7,7 +7,7 @@
    * Set to the following to disable:
    *   const welcomeJournal = '';
    */
-  const welcomeJournal = '_Overview';
+  const welcomeJournal = 'Hundholt';
   /**
    * additionalJournals will automatically be imported.
    * Set to the following to disable:
